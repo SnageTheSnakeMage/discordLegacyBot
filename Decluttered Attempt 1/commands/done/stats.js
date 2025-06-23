2515fb45a67303daf2dec7cee94896790d3e85ea
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2025-06-22 17:13:51 -0700

    leftover

--- Decluttered Attempt 1/commands/done/stats.js
+++ Decluttered Attempt 1/commands/done/stats.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const utils = require('../utils');
+const utils = require('../../utils');
 var models = utils.models;
 
 module.exports = {
