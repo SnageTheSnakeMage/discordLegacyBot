fb7e453cde6e9eb6e9dec54bb16424544f313641
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2026-08-29 21:48:27 -0700

    update gift test with new setup

--- DeclutteredAttempt1/tests/commands/Player Commands/gift.test.js
+++ DeclutteredAttempt1/tests/commands/Player Commands/gift.test.js
@@ -1,25 +1,22 @@
 /**
  * Unit tests for the gift command.
  */
-const { createMockInteraction } = require('../../helpers/mockInteraction');
-const { createFakeGame, createFakePlayer } = require('../../helpers/mockUtils');
-
-jest.mock('discord.js', () => require('../../helpers/mockDiscord').createDiscordMock());
-jest.mock('../../../utils', () => require('../../helpers/mockUtils').createUtilsMock());
-
+jest.mock('../../../utils');
+const { createFakeGame, createFakePlayer } = require('../../helpers/mockModels')
 const utils = require('../../../utils');
 const gift = require('../../../commands/Player Commands/gift');
 
 describe('gift command', () => {
   beforeEach(() => {
     jest.clearAllMocks();
-    utils.getOldestActiveGameId.mockResolvedValue(1);
-    utils.checkGameState.mockResolvedValue(false);
+    jest.mock('../../../utils');
+    jest.spyOn(utils, "getOldestActiveGameId").mockResolvedValue(1);
+    jest.spyOn(utils,"checkGameState").mockResolvedValue(false);
     const game = createFakeGame({ Game_ID: 1 });
     const player = createFakePlayer({ Game_ID: 1, Discord_ID: '123', Dead: false });
     const recipient = createFakePlayer({ Game_ID: 1, Discord_ID: '456' });
-    utils.models.Games.findByPk.mockResolvedValue(game);
-    utils.models.Players.findOne.mockImplementation(({ where }) => {
+    jest.spyOn(utils.models.Games,"findByPk").mockResolvedValue(game);
+    jest.spyOn(utils.models.Players,"findOne").mockImplementation(({ where }) => {
       if (where?.Discord_ID === '123') return Promise.resolve(player);
       if (where?.Discord_ID === '456') return Promise.resolve(recipient);
       return Promise.resolve(null);
