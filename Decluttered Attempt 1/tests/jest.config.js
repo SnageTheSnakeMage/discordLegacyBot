9c83ab12ca37b369fac9e094d14047423ee68532
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2026-09-13 16:04:20 -0700

    removed stale directory

--- Decluttered Attempt 1/tests/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverageFrom: ['commands/**/*.js', 'utils.js'],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules/']
-};
